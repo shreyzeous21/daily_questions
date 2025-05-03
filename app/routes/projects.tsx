26c52cbb0169047f7849f978cb95043ff95f2cf4
Shrey_sadhukhan <shrey.sadhukhan21@gmail.com>
2025-05-03 14:19:49 +0530

    projects.tsx

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -25,7 +25,7 @@ interface iAppProps {
 export async function loader({}: LoaderArgs) {
   const query = gql`
     query MyQuery {
-      projects(orderBy: publishedAt_DESC) {
+      projects(orderBy: publishedAt_DESC,first: 1000) {
         id
         link
         overview
